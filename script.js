87be0cdf7bb82318756fdb406466cd824c3b5abd
ANTHONY STEMATZ-BREITLING <stembreit@gmail.com>
2021-03-12 19:46:17 -0600

    further refactoring of code

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
-//an object to represent the day planner
-var Schedule = {
-    hourText: ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"],
-    inputEntrees: {
-        9: " ",
-        10: " ",
-        11: " ",
-        12: " ",
-        13: " ",
-        14: " ",
-        15: " ",
-        16: " ",
-        17: " ",
-    },
+//array to show in time slot
+let hourText = ["9:00AM", "10:00AM", "11:00AM", "12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM"]
+//set inputEntrees to object from local storage
+//or object representing the military times and associated inputs
+let inputEntrees = JSON.parse(localStorage.getItem("plans")) || {
+    9: " ",
+    10: " ",
+    11: " ",
+    12: " ",
+    13: " ",
+    14: " ",
+    15: " ",
+    16: " ",
+    17: " ",
 }
-
-//retrieving saved inputs from local storage and set in correct timeslot
-RetreivePlans();
-function RetreivePlans() {
-    //get object from local storage 
-    var scheduledPlans = JSON.parse(localStorage.getItem("plans"));
-    //update Schedule object with object from local storage
-    Schedule.inputEntrees = scheduledPlans;
-};
-
-//defines i so that it can be used in loop
-let i = 0;
-// loops through the Schedule.inputEntrees object 
-Object.keys(Schedule.inputEntrees).forEach(function (value) {
-    //creates html for the page
+//return array of object keys and loops for each key
+Object.keys(inputEntrees).forEach(function (key) {
+    //set a variable of i since the keys start at 9 and increment up
+    //I can now access the hourText array using i like in a for loop
+    let i = key - 9
+    //creates a div to hold an hour
     let divrow = $("<div></div>").addClass("time-block input-group mb-3");
-    let timeslot = $("<span></span>").text(Schedule.hourText[i]).addClass("input-group-prepend input-group-text");
-    let inputSection = $("<input></input>").addClass("input form-control").attr("id", value).attr("data-input", value);
-    //sets the input value separatly so it can be seen by user
-    inputSection.val(Schedule.inputEntrees[value])
-    let savebtn = $("<button class='saveBtn input-group-append input-group-text'>Save</button>").attr("value", value);
-    //appends time input and button into di
+    //creates the time span setting text from the hourText array
+    let timeslot = $("<span></span>").text(hourText[i]).addClass("input-group-prepend input-group-text");
+    //creates input with key with an id and data-input matching the key 
+    let inputSection = $("<input></input>").addClass("input form-control").attr("id", key).attr("data-input", key);
+    //updates the value of that input with matching value in inputEntrees
+    inputSection.val(inputEntrees[key])
+    //create save button and adds a defined value of the key
+    let savebtn = $("<button class='saveBtn input-group-append input-group-text'>Save</button>").attr("value", key);
+    //appends the three sections to the divrow
     divrow.append(timeslot);
     divrow.append(inputSection);
     divrow.append(savebtn);
-    //appends each div inside container
+    //appends the divrow to the contianer div in the html
     $(".container").append(divrow);
-    //increments i so that it can be used in loop
-    i++
 });
 
-//taking the current date and dictating how it is displayed
+//gets the html by id and sets the text to the current day using momentJS
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 //testing current hour against input hour value
@@ -60,19 +52,19 @@ function testMoment(currentHour) {
     }
 };
 
-//running through each input and executing testMoment function
+//running through each input
 $("input").each(function () {
-    //changing the class of the current input to the return of that function
+    //takes that input and adds a class
+    //dependent on the return from testMoment
     $(this).addClass(testMoment($(this).data('input')));
 });
 
-//sending input values with thier id to local storage
+//when any button is clicked
 $("button").click(function (e) {
-    //grabs the buttons value
+    //gets the value of that button
     let id = e.target.value
-    //updates the Schedule object with the inputs values
-    //the input id matches the buttons given value
-    Schedule.inputEntrees[id] = $("#" + id).val().trim()
-    //puts new object into local storage
-    localStorage.setItem("plans", JSON.stringify(Schedule.inputEntrees));
+    //updates inputEntrees with the new input value
+    inputEntrees[id] = $("#" + id).val().trim()
+    //puts inputEntrees into local storage
+    localStorage.setItem("plans", JSON.stringify(inputEntrees));
 });
